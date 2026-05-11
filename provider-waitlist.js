e3908c551d67fc5812fbc4f24fdec6deb0ce6c60
Thomas <thomaskangai@ymail.com>
2026-05-11 17:08:46 +0200

    Moved lanuch date to june 12 th

--- provider-waitlist.js
+++ provider-waitlist.js
@@ -12,7 +12,7 @@
     brandName: "fixZa",
     city: "Your City",
     // ISO strings in local-interpreted as Date (use explicit Z if you need UTC)
-    launchDate: "2026-05-15T08:00:00",
+    launchDate: "2026-06-12T08:00:00",
     waitlistCloseDate: "2026-05-01T23:59:59",
     waitlistCount: 14,
     founderSlots: 20,
